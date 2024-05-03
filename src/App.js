223759def24f8079a5bced3dcac6f23650cf431a
Djeb94 <151559648+Djeb94@users.noreply.github.com>
2024-05-03 05:21:58 +0200

    Features 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importez Routes au lieu de Switch
+import {Fragment} from 'react';
+import "./App.css";
+import "./fonts/Loomattic-Regular.woff2";
+import "./fonts/Nunito-Regular.woff2";
+import Main from './components/main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment>
+        <Routes> {/* Utilisez Routes au lieu de Switch */}
+          <Route path="/" element={<Main />} /> {/* Utilisez element au lieu de component */}
+        </Routes>
+        </Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/main.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import "./main.css";
+
+function Main() {
+  const [showSubMenu, setShowSubMenu] = useState(false);
+
+  const toggleSubMenu = () => {
+    setShowSubMenu(!showSubMenu);
+  };
+
+  return (
+    <div>
+      <div className="navbar">
+        <a>DISCOVER</a>
+        <a>FEATURES</a>
+        <a>SEASONS</a>
+        <button id="login">&#x2022; Log In &#x2022;</button>
+        <button id="signup2">&#x2022; Sign Up &#x2022;</button>
+        <span className="material-symbols-outlined" id="toggle-menu" onClick={toggleSubMenu}>
+          menu
+        </span>
+        {showSubMenu && (
+          <div id="toogle-nav">
+          <p>DISCOVER</p>
+        <p>FEATURES</p>
+        <p>SEASONS</p>
+        <button>space</button>
+        <div id="toogle-login">&#x2022; Log In &#x2022;</div>
+        <div id="toogle-signup">&#x2022; Sign Up &#x2022;</div>
+          </div>
+        )}
+      </div>
+
+      <div className="containeur">
+        <div className="element1">
+          <h2>SPACE <mark>KANGURUS</mark></h2>
+          <p>Warp into a epic space adventure & defend the motherland against the cyber dingoes !</p>
+          <button id="steam">&#x2022; Wish list on Steam &#x2022;</button>
+          <button id="signup">&#x2022; Sign Up &#x2022;</button>
+        </div>
+        <div className="element2"></div>
+      </div>
+      
+      <div className="footer">
+        <div className="sous-menu">
+          <p id="first-menu">HOME</p>
+          <p>DISCOVER</p>
+          <p>FEATURES</p>
+          <p>SEASONS</p>
+          <p id="logout">LOG OUT</p>
+        </div>
+        <div>
+          <button id="steam2">&#x2022; Wish list on Steam &#x2022;</button>
+        </div>
+        <div className="footer-right">
+          <p id="followus">FOLLOW US</p>
+          <div id="reseau"></div>
+          <p id="footer-text">Lorem ipsum dolor sit amet. Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labor et</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
